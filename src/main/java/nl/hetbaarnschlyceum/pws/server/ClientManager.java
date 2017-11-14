2d4d9e41d42e5cdde009a09e9a02c02379f3069a
TheNLGamerZone <timmetje15@hotmail.nl>
2017-11-14 16:15:02 +0100

    Begonnen aan clientmanager

---
+++ src/main/java/nl/hetbaarnschlyceum/pws/server/ClientManager.java
@@ -0,0 +1,13 @@
+package nl.hetbaarnschlyceum.pws.server;
+
+public class ClientManager {
+    public int registerClient(String name, int number, String hash)
+    {
+        return 0;
+    }
+
+    private boolean checkAvailable(int number)
+    {
+        return false;
+    }
+}
